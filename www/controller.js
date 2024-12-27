307b4ec6a768dcccedc3bf74aeeb687cd5a5453d
mion007 <mionmax45@gmail.com>
2024-12-27 11:40:24 +0530

    ju

--- www/controller.js
+++ www/controller.js
@@ -1,14 +1,10 @@
 $(document).ready(function () {
 
-
-
     // Display Speak Message
     eel.expose(DisplayMessage)
     function DisplayMessage(message) {
-
         $(".siri-message li:first").text(message);
         $('.siri-message').textillate('start');
-
     }
 
     // Display hood
@@ -34,7 +30,6 @@ $(document).ready(function () {
 
     eel.expose(receiverText)
     function receiverText(message) {
-
         var chatBox = document.getElementById("chat-canvas-body");
         if (message.trim() !== "") {
             chatBox.innerHTML += `<div class="row justify-content-start mb-4">
@@ -46,50 +41,29 @@ $(document).ready(function () {
             // Scroll to the bottom of the chat box
             chatBox.scrollTop = chatBox.scrollHeight;
         }
-        
     }
 
-    
     // Hide Loader and display Face Auth animation
     eel.expose(hideLoader)
     function hideLoader() {
-
         $("#Loader").attr("hidden", true);
         $("#FaceAuth").attr("hidden", false);
-
     }
+
     // Hide Face auth and display Face Auth success animation
     eel.expose(hideFaceAuth)
     function hideFaceAuth() {
-
         $("#FaceAuth").attr("hidden", true);
         $("#FaceAuthSuccess").attr("hidden", false);
-
     }
-    // Hide success and display 
+
+    // Hide success and display HelloGreet animation
     eel.expose(hideFaceAuthSuccess)
     function hideFaceAuthSuccess() {
-
         $("#FaceAuthSuccess").attr("hidden", true);
         $("#HelloGreet").attr("hidden", false);
-
     }
 
-
-    // Hide Start Page and display blob
-    eel.expose(hideStart)
-    function hideStart() {
-
-        $("#Start").attr("hidden", true);
-
-        setTimeout(function () {
-            $("#Oval").addClass("animate__animated animate__zoomIn");
-
-        }, 1000)
-        setTimeout(function () {
-            $("#Oval").attr("hidden", false);
-        }, 1000)
-    }
-
-
+    // Directly show the Oval section on page load
+    ShowHood();
 });
